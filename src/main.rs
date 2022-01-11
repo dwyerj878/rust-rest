0897993df966d753533d4a93c2dfdd52f5c4074f
James Dwyer <dwyerj878@gmail.com>
2022-01-10 22:10:28 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+use json::object;
+use json::array;
+use warp::Filter;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "server")]
+struct Cli {
+    // A flag, true if used in the command line. Note doc comment will
+    // be used for the help message of the flag. The name of the
+    // argument will be, by default, based on the name of the field.
+    /// Activate debug mode
+    #[structopt(short, long)]
+    debug: bool,
+
+    // The number of occurrences of the `v/verbose` flag
+    /// Verbose mode (-v, -vv, -vvv, etc.)
+    #[structopt(short, long, parse(from_occurrences))]
+    verbose: u8,
+
+    /// Set port
+    #[structopt(short, long, default_value = "5000")]
+    port: u16,
+
+
+    /// bind address
+    #[structopt(short, long, default_value = "127.0.0.1")]
+    bind: String,
+
+    /// Files to process
+    #[structopt(name = "FILE", parse(from_os_str))]
+    files: Vec<PathBuf>,
+}
+
+
+fn main() {
+    
+    let opt = Cli::from_args();
+    println!("{:#?}", opt);
+
+    let data = object!{
+        "code" => 200,
+        "success" => true,
+        "payload" => object!{
+            "features" => array![
+                "awesome",
+                "easyAPI",
+                "lowLearningCurve"
+            ]
+        }
+    };
+
+    println!("{:#?}", data.dump());
+    println!("{}", data["code"]);
+    println!("Start");
+    start_http(opt.port, opt.bind);
+    println!("Stop");
+}
+
+#[tokio::main]
+async fn start_http(port: u16, bind :String) {
+    let root = warp::path::end().map(|| "Welcome to my warp server!");
+    let routes = root.with(warp::cors().allow_any_origin());
+
+    let bind_addr = bind.parse::<Ipv4Addr>().unwrap();
+    let socket = SocketAddr::new(IpAddr::V4(bind_addr), port);
+
+    
+    warp::serve(routes).run(socket).await;
+}
